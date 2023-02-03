71c762f558759cc3734a1b34327ebae323931446
WIN-QV0R9JHOTKJ\Administrator <123>
2023-02-04 01:06:42 +0800

    feat: 增加less 和 目录结构

--- craco.config.js
+++ craco.config.js
@@ -1,43 +1,44 @@
 const path = require('path');
-const { name } = require('./package.json');
-
+const CracoLessPlugin = require('craco-less');
 const pathResolve = pathUrl => path.join(__dirname, pathUrl);
 
 module.exports = {
-  reactScriptsVersion: 'react-scripts' /* (default value) */,
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: { '@primary-color': '#1DA57A' },
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
   webpack: {
     alias: {
       '@': pathResolve('src'),
-      '@assets': pathResolve('src/assets'),
-      '@components': pathResolve('src/components'),
-      '@constants': pathResolve('src/constants'),
-      '@containers': pathResolve('src/containers'),
-      '@hooks': pathResolve('src/hooks'),
-      '@mocks': pathResolve('src/mocks'),
-      '@routes': pathResolve('src/routes'),
-      '@services': pathResolve('src/services'),
-      '@styles': pathResolve('src/styles'),
-      '@types': pathResolve('src/types'),
-      '@utils': pathResolve('src/utils'),
-      '@contexts': pathResolve('src/contexts'),
-    },
-    configure(webpackConfig) {
-      // 配置扩展扩展名
-      webpackConfig.resolve.extensions = [...webpackConfig.resolve.extensions, ...['.scss', '.css']];
-      // 接入微前端框架qiankun的配置,不接入微前端可以不需要
-      webpackConfig.output.library = `${name}-[name]`;
-      webpackConfig.output.libraryTarget = 'umd';
-      webpackConfig.output.globalObject = 'window';
-      return webpackConfig;
     },
   },
   devServer: {
-    // 本地服务的端口号
-    port: 3001,
-    // 本地服务的响应头设置
-    headers: {
-      // 允许跨域
-      'Access-Control-Allow-Origin': '*',
+    port: 3600,
+    hot: true,
+    open: false,
+    proxy: {
+      '/api': {
+        target: 'http://172.16.18.160:9000/srm',
+        changeOrigin: true,
+        pathRewrite: { '^/api': '' },
+      },
+      '/data_api': {
+        target: 'http://172.16.18.160:9000/v1/DataCenter',
+        changeOrigin: true,
+        pathRewrite: { '^/data_api': '' },
+      },
     },
+  },
+  typescript: {
+    enableTypeChecking: true, //是否开启类型检查
   },
 };
---
+++ src/componenets/Demo/index.tsx
@@ -0,0 +1,8 @@
+const Demo = () => {
+  return (
+    <div>
+      <h1>hello world</h1>
+    </div>
+  );
+};
+export default Demo;
