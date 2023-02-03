6e1f0f616643e20e11334a77edfa39bc327fadb8
WIN-QV0R9JHOTKJ\Administrator <123>
2023-02-03 23:43:43 +0800

    husky

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-const a114 = 123;
+const a1141 = 123;
 root.render(
   <React.StrictMode>
     <App />
